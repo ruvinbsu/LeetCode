963c3043b18ed3ddcb4b86915e0587db565c2bf5
ruvinyusubov <ruvinbsu@gmail.com>
2017-10-22 17:00:02 -0400

    Reverse String 344

---
+++ src/com/leetcode/stringmanipulation/easy/ReverseString_344.java
@@ -0,0 +1,10 @@
+package com.leetcode.stringmanipulation.easy;
+
+/**
+ * Created by ruvinyusubov on 22/10/2017.
+ */
+public class ReverseString_344 {
+    public String reverseString(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+}
