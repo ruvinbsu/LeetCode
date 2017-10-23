2cfede7d6f8ecac821488712747688d8b873c9ab
ruvinyusubov <ruvinbsu@gmail.com>
2017-10-22 21:33:26 -0400

    Maximum Depth of Binary Tree 104

---
+++ src/com/leetcode/datastructure/tree/easy/MaximumDepthofBinaryTree_104.java
@@ -0,0 +1,33 @@
+package com.leetcode.datastructure.tree.easy;
+
+import apple.laf.JRSUIUtils;
+
+/**
+ * Created by ruvinyusubov on 22/10/2017.
+ */
+public class MaximumDepthofBinaryTree_104 {
+    public int maxx = 0;
+
+    public int maxDepth(TreeNode root) {
+        inOrderTraversal(root, 1);
+        return maxx;
+    }
+
+    public void inOrderTraversal(TreeNode node, int curr)
+    {
+        if (node == null)
+            return;
+
+        if (node.left == null && node.right == null && curr > maxx){
+            maxx = curr;
+        }
+
+        inOrderTraversal(node.left, curr + 1);
+        inOrderTraversal(node.right, curr + 1);
+    }
+}
+
+class TreeNode {
+    public TreeNode left;
+    public TreeNode right;
+}
